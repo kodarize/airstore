a07203f17703afb1bff6fd10b4f96c73035b6db2
John C. Barr <Farwalker3@icloud.com>
2023-03-21 11:31:39 -0400

    Alpha 0.02
    A0.02

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
 // Connection to a broadcast channel
 const bc = new BroadcastChannel('test_channel');
-
-// Example of sending of a very simple message
-// It doesn't have to be a string, it could be a JS object
