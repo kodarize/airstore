e132c20a565fea02abe7098bdcdd97374da6e35a
John C. Barr <Farwalker3@icloud.com>
2023-03-22 23:23:18 -0400

    Alpha 0.04
    A0.04

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Connection to a broadcast channel
 const bc = new BroadcastChannel('test_channel');
+
+function changeApp(AppValue) {
+	const bc=new window.BroadcastChannel('test_channel');
+	bc.postMessage(AppValue);
+}
